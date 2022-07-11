ec5389fcabeeaf032664eebb6728fa7b8402fc46
DMarcRQ <marc.dolbec@revenuquebec.ca>
2022-07-11 15:55:41 -0400

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -11,9 +11,15 @@ export class ProductDetailsComponent implements OnInit {
 
   product : Product | undefined;
 
-  constructor() { }
+  constructor(private route :  ActivatedRoute) { }
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
